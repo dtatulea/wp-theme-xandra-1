4c14773ba62792faaf602390b8d6c52aba65bd17
Dragos Tatulea <dragos@dragos-tp.(none)>
2011-06-05 17:25:27 +0300

    removed page name from page.php
    Signed-off-by: Dragos Tatulea <dragos.tatulea@gmail.com>

--- page.php
+++ page.php
@@ -6,7 +6,6 @@
 <?php the_post(); ?>
 				
 				<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<h1 class="entry-title"><?php the_title(); ?></h1>
 					<div class="entry-content">
 <?php the_content(); ?>
 <?php wp_link_pages('before=<div class="page-link">' . __( 'Pages:', 'your-theme' ) . '&after=</div>') ?>					
