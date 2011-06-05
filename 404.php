32b3d8eeb2b6b27be8941dec18a33d6e06449cf4
Dragos Tatulea <dragos@dragos-tp.(none)>
2011-06-05 11:53:14 +0300

    Changed 404 message & removed search.
    Signed-off-by: Dragos Tatulea <dragos.tatulea@gmail.com>

--- 404.php
+++ 404.php
@@ -6,8 +6,7 @@
 				<div id="post-0" class="post error404 not-found">
 					<h1 class="entry-title"><?php _e( 'Not Found', 'your-theme' ); ?></h1>
 					<div class="entry-content">
-						<p><?php _e( 'Apologies, but we were unable to find what you were looking for. Perhaps searching will help.', 'your-theme' ); ?></p>
-	<?php get_search_form(); ?>
+						<p><?php _e( 'You are stressing the fabric of reality...'); ?></p>
 					</div><!-- .entry-content -->
 				</div><!-- #post-0 -->
 
